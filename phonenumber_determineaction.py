30c9aa7386d5e497d8bde31e92151372382bcff9
Gregory Smith <gregjsmith@gmx.com>
2020-07-02 11:25:08 -0400

    Clean up {phone_determineaction.py}

--- phonenumber_determineaction.py
+++ phonenumber_determineaction.py
@@ -4,10 +4,7 @@
 import os
 import re
 
-#Use this class to create what you will send to the phonenumbers dictionary
-#Store what you will send the 3 bills in a list
-#(These numbers should contain the bills/what you pulled from govtrack.us)
-
+# Recieve bills from {billparser.py} > Remove last bill > Send to phone {sendtophone.py} > Write to #.txt
 
 class c_phonenumbersdetermineaction :
 
@@ -15,7 +12,6 @@ class c_phonenumbersdetermineaction :
         list_phonenumbers = [19145821666, 19145824751, 19147037991]
         self.list_phonenumbers = list_phonenumbers
     
-    #Method to determine what do
     def f_actionfornumber (self, arg_stringmessage) :
         
         self.string_message = arg_stringmessage
@@ -23,32 +19,20 @@ class c_phonenumbersdetermineaction :
             #If File #.txt exists
             if (os.path.exists("/home/greg/Projects/Projects/python/BillParser_Project/phonenumber_files/{0}.txt".format(i)) ) :
                 try :
-                    #Send the top 3 from that file and then remove them
                     file_obj = open ("/home/greg/Projects/Projects/python/BillParser_Project/phonenumber_files/{0}.txt".format(i), "r")
-                    #Contains txt file
                     str_fromfile = file_obj.read()
-                    #Move into list, remove first element and send to phone and return to list with the one element removed
                     l_fromfile = str_fromfile.split('", "')
                     l_lastelement = l_fromfile.pop()
                     file_obj.close()
-                    #print (l_lastelement)
                     #sendtophonefile (l_lastelement) # <-- contains the last bill
-
-                    
 
                     # Delete File #
                     os.remove("/home/greg/Projects/Projects/python/BillParser_Project/phonenumber_files/{0}.txt".format(i))
                     # Create File #
-                    file_objwrite = open ("/home/greg/Projects/Projects/python/BillParser_Project/phonenumber_files/{0}.txt".format(i), "w")
-                    # Write to File List minus last element #
-                    # Remove \
-                    #re.sub ("\\",'',str(l_fromfile))
-                    
-                    file_objwrite.write(str(l_fromfile).replace("\\",""))
+                    file_objwrite = open ("/home/greg/Projects/Projects/python/BillParser_Project/phonenumber_files/{0}.txt".format(i), "w")                 
+                    file_objwrite.write(str(l_fromfile).replace("\\","")) # Remove \
                     file_objwrite.close()
-                    # Send to phone the last list element #
-                    
-                    
+                                        
                 except IOError :
                     print ("Error! Problem Opening File", IOError)
 
